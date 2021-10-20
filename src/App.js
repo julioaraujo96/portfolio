7b4bab0bf553143514fcb28f511c0bf2c896f4e5
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2021-10-20 16:00:10 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Navbar from './components/Navbar/Navbar';
+import './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Helmet>
+        <meta name="description" content="Personal Portfolio"/>
+        <title>Júlio Araújo - Homepage</title>
+    </Helmet>
+    <Navbar/>
+    </>
   );
 }
 
