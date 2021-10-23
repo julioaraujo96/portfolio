1715c5ea03be34a1a57be50b279278bb937ab00f
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2021-10-23 14:16:25 +0100

    navbar icons adjustment

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import GithubLogo from '../../UI/Logo/Github/GithubLogo';
+import Darkmode from '../../UI/Darkmode/Darkmode';
+import GithubLogo from '../../UI/Github/GithubLogo';
+import Hamburger from '../../UI/Hamburguer/Hamburger';
 import Logo from '../../UI/Logo/Logo';
 
 const Navbar = () => 
 {
     return (
-        <nav className="bg-white shadow-lg">
+        <nav className="bg-white border-b-2">
             <div className="max-w-6xl mx-auto px-4">
                 <div className="flex justify-between">
                      <div className="flex space-x-7">
@@ -16,16 +18,17 @@ const Navbar = () =>
                                 <a href="#" className="py-4 px-2 text-primary border-b-4 border-primary font-semibold ">
                                     Home
                                 </a>
-                                <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-primary transition duration-300">
+                                <a href="" className="py-4 px-2 text-gray-500 border-b-4 border-transparent font-semibold hover:text-primary transition duration-300">
                                     Projects
                                 </a>
                          </div>
-
-                            {/* Navegação Secundária */}
-                        <div className="hidden md:flex items-center space-x-3 ">
-                            <GithubLogo/>
-                        </div>
                     </div>
+                      {/* Navegação Secundária */}
+                      <div className="hidden md:flex items-center space-x-3">
+                            <GithubLogo/>
+                            <Darkmode />
+                       </div>
+                        <Hamburger />
                 </div>
             </div>
         </nav>
