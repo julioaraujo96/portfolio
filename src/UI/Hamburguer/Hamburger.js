c092af6d9e4cb6a9cd66b25cb84068f1be280f81
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2021-10-23 22:47:24 +0100

    updated navbar

--- src/UI/Hamburguer/Hamburger.js
+++ src/UI/Hamburguer/Hamburger.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
-const Hamburger = () => {
+const Hamburger = (props) => {
     return (
-        <div class="md:hidden flex items-center">
-        <button class="outline-none mobile-menu-button">
-            <svg
-                class="w-6 h-6 text-gray-500"
-                x-show="!showMenu"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-            <path d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-        </button>
-    </div>
+        <>
+        <div className="md:hidden flex items-center">
+	        <button className="outline-none mobile-menu-button" onClick={props.menuToggle}>
+				<svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+				    x-show="!showMenu"
+					fill="none"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="2"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+			        <path d="M4 6h16M4 12h16M4 18h16">
+
+                    </path>
+				</svg>
+					</button>
+		    </div>
+			
+			<div className={props.showMenu ? "hidden" : ""}>
+				<ul className="">
+					<li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
+					<li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Projects</a></li>
+				</ul>
+			</div> 
+        </>
     );
 };
 
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,38 +1,126 @@
-import React from 'react'
-import Darkmode from '../../UI/Darkmode/Darkmode';
-import GithubLogo from '../../UI/Github/GithubLogo';
-import Hamburger from '../../UI/Hamburguer/Hamburger';
-import Logo from '../../UI/Logo/Logo';
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import Logo from "../../UI/Logo/logo.png";
+import GithubLogo from '../../UI/Github/GithubLogo'
+import Darkmode from "../../UI/Darkmode/Darkmode";
+const Navbar = () => {
 
-const Navbar = () => 
-{
-    return (
-        <nav className="bg-white border-b-2">
-            <div className="max-w-6xl mx-auto px-4">
-                <div className="flex justify-between">
-                     <div className="flex space-x-7">
-                            <Logo />
+  const [isOpen, setIsOpen] = useState(false);
 
-                            {/* Navegação Primária */}
-                         <div className="hidden md:flex items-center space-x-1">
-                                <a href="#" className="py-4 px-2 text-primary border-b-4 border-primary font-semibold ">
-                                    Home
-                                </a>
-                                <a href="" className="py-4 px-2 text-gray-500 border-b-4 border-transparent font-semibold hover:text-primary transition duration-300">
-                                    Projects
-                                </a>
-                         </div>
-                    </div>
-                      {/* Navegação Secundária */}
-                      <div className="hidden md:flex items-center space-x-3">
-                            <GithubLogo/>
-                            <Darkmode />
-                       </div>
-                        <Hamburger />
+  return (
+    <>
+      <nav className="bg-white border-b-2">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8"
+                  src={Logo}
+                  alt="Personal Logo"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className=" hover:bg-gray-200 text-secondary px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-secondary hover:bg-gray-200 px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </a>
                 </div>
+              </div>
             </div>
-        </nav>
-    )
+            <div class="hidden md:flex items-center space-x-3 ">
+                <GithubLogo />
+                <Darkmode />
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-primary"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className=" hover:bg-gray-200 text-secondary block px-3 py-2 rounded-md text-base font-medium hover:text-primary"
+                >
+                  Home
+                </a>
+
+                <a
+                  href="#"
+                  className="text-secondary hover:bg-gray-200 hover:text-primary block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
