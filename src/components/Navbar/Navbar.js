80742c8ef96a40fedaa661209181075bf5dffcf2
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2021-11-02 22:36:47 +0000

    hire button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@
 
     return (
       <>
-        <nav className="bg-white border-b-2 dark:border-primary dark:bg-secondary transition duration-500">
+        <nav className="sticky top-0 z-50 bg-white border-b-2 dark:border-primary dark:bg-secondary transition duration-500">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
               <div className="flex items-center">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,19 @@ import React from 'react';
 const Home = () => {
     return (
         <section className="container h-screen mx-auto">
-            <div className="bg-gray-300 rounded px-2 py-2 text-center my-5 dark:bg-gray-500">
-                 <h1 className="font-bold sm:text-xl md:text-2xl lg:text-3xl text-secondary dark:text-white"> Hello, I'm a Developer based in Porto.</h1>
+            <div className="flex justify-between"> 
+                <h2 className="text-primary my-10 lg:text-3xl md:text-2xl sm:text-xl px-5">Welcome to my Portfolio</h2>
+                <a 
+                    class="bg-primary hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 h-10 self-center mx-2 rounded"
+                    href="mailto:someone@something.com?subject=your title&body=TThe message"
+                >
+                     Hire me
+                </a>
+            </div>
+            <div className="mx-2 bg-gray-300 rounded px-2 py-2 text-center my-20 dark:bg-gray-700">
+                 <h1 className="font-light sm:text-base md:text-lg lg:text-xl text-secondary dark:text-white">
+                    I am <span className="text-primary">Júlio Araújo</span> - A Developer based in Porto.
+            </h1>
             </div>
         </section>
     );
