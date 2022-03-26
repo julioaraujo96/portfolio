89d7bd4e617cd34b3eecd30310deec2640a62898
julioaraujo96 <49203294+julioaraujo96@users.noreply.github.com>
2022-03-26 15:00:04 +0000

    button update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typewriter } from 'react-simple-typewriter'
 import {Link}  from 'react-router-dom';
+import {AiOutlineMail} from 'react-icons/ai'
 
 const Home = () => {
     return (
@@ -8,10 +9,10 @@ const Home = () => {
             <div className="flex justify-between mb-20"> 
                 <h2 className="text-primary my-10 lg:text-3xl md:text-2xl sm:text-xl px-5">Welcome to my Portfolio</h2>
                 <a 
-                    class="bg-primary hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 h-10 self-center mx-2 rounded text-xs md:text-base lg:text-lg flex-1 text-center custom-max-w-9"
+                    class="bg-primary hover:bg-blue-400 text-white flex items-center font-bold border-b-4 border-blue-700 hover:border-blue-500 h-10 self-center mx-2 rounded text-xs flex-1 custom-max-w-9"
                     href="mailto:hire.julio96@gmail.com"
                 >
-                     Hire me
+                     <p className="flex mx-auto items-center gap-2">Hire <AiOutlineMail /></p> 
                 </a>
             </div>
             <div className="bg-gray-300 rounded px-2 py-2 text-center mx-2 dark:bg-gray-700">
